acbf9aa5ee44a5905c9ddd93baeddcc02c26b27d
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-06 21:33:59 -0700

    javascript

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
-$(document).ready(function() {
+const key = 'cywm9F3bjJCv5L6vBDxE6Y8xJ26x0LKz';
 
-    // when user clicks search button the input will empty
-    $('#searchButton').on('click', function(e) {
-        e.preventDefault();
-        $('input').val('');
-    })
-    
-    // function to change what is displayed in the #recipesEl section
-    function changeDisplay() {
-        $('#searchResults').toggleClass('hide');
-        $('#dogparkDisplay').toggleClass('hide');
-    }
-    
-    // when user clicks a recipe title from the search results the recipe will display
-    $('#searchResults').on('click', 'h2', function() {
-        changeDisplay();
-    });
-    
-    // when user clicks the backButton the display will go back to search results
-    $('#backButton').on('click', function() {
-        changeDisplay();
-    })
-    
-    });
+placeSearch({
+    key: 'KEY',
+    container: document.querySelector('#place-search-input')
+  });
