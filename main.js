c56819e0ef7730ce52dd7614de9dca05b247f02d
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-07 06:50:07 -0700

    javascript

--- main.js
+++ main.js
@@ -1,31 +1,9 @@
-const key = 'cywm9F3bjJCv5L6vBDxE6Y8xJ26x0LKz';
-
-let searchResults = $('.place-search-input').val();
-$(document).ready(function(){
-
-$('.searchButton').on('click', function(){
-    $('.place-search-input').val('');
-})
-const file = log,
-log = new Log('info');
-var queryUrl = 'http://www.mapquestapi.com/search/v2/radius?&key=cywm9F3bjJCv5L6vBDxE6Y8xJ26x0LKz'
-$.ajax({
-    url: queryUrl,
-    dataType: 'json',
-    type: 'POST',
-    contentType:'json',
-    data: {
-        key: decodeURIComponent(key),
-        json : JSON.stringify( 
-            {
-                location: { "postalCode": "98765"}, options: { thumbMaps: false}
-            }) 
-    },
-    success: function(data) { log( data ) },
-    error: function(data) { log( 'error occurred - ' + zipCode + ' - ' + data ) },
+const key = 'AIzaSyCxe3ZrTFj6i92x-duy3G9Sb0R2cJrB_nc';
+const input = 'text';
+const name = 'name';
 
 
-   
-});
-      
-});
+
+
+
+
