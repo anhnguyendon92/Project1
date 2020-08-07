4ebc3ce6030a50ccf6b6c29106f9cff28b736f29
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-07 07:30:09 -0700

    jquery

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-const key = 'AIzaSyCxe3ZrTFj6i92x-duy3G9Sb0R2cJrB_nc';
-const input = 'text';
-const name = 'name';
+//const key = 'AIzaSyCxe3ZrTFj6i92x-duy3G9Sb0R2cJrB_nc';
+var map;
+
+function createMap() {
+    var options = {
+        center: { lat: 43.654, lng: -79.383 },
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.Hybrid,
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), options);
+
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function () {
+            var position = {
+                lat,
+                lng
+            }
+            function () {
+                handleLocationError('Geolocation service failed', map.center())
+
+                
+            
+            
+        })
+    } else {
+        handleLocationError('No geolocation available', map.center())
+
+        
+    }
+
+    function handleLocationError(content, position) {
+        info.window.setPosition(position);
+        infoWindow.setContent(content);
+        infoWindow.open(map);
+    }
+}
+
 
 
 
