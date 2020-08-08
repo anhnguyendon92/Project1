dbdc6ff9ad641d6fdcb531000467e52b3ad3d3a0
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-08 13:56:49 -0700

    adding to javascript

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//const key = 'AIzaSyCxe3ZrTFj6i92x-duy3G9Sb0R2cJrB_nc';
+const key = 'AIzaSyCxe3ZrTFj6i92x-duy3G9Sb0R2cJrB_nc';
 var map;
 
 function createMap() {
@@ -15,28 +15,24 @@ function createMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function () {
             var position = {
-                lat,
-                lng
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             }
-            function () {
-                handleLocationError('Geolocation service failed', map.center())
+            }, function () {
+            handleLocationError('Geolocation service failed', map.center());
+            })
+            } else {
+                handleLocationError('No geolocation available', map.center());
 
-                
-            
-            
-        })
-    } else {
-        handleLocationError('No geolocation available', map.center())
+            }
+            }
 
+            function handleLocationError(content, position) {
+                info.window.setPosition(position);
+                infoWindow.setContent(content);
+                infoWindow.open(map);
+            }
         
-    }
-
-    function handleLocationError(content, position) {
-        info.window.setPosition(position);
-        infoWindow.setContent(content);
-        infoWindow.open(map);
-    }
-}
 
 
 
