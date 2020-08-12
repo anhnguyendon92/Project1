63d72b2c95e6ec31f411cd4fcbdac8eee8d4f07c
Anh Nguyen <anhnguyendon92@gmail.com>
2020-08-11 23:15:21 -0700

    Done with 1 API

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
-const key = 'AIzaSyCxe3ZrTFj6i92x-duy3G9Sb0R2cJrB_nc';
-var map;
 
 function createMap() {
-    var options = {
-        center: { lat: 43.654, lng: -79.383 },
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.Hybrid,
-    };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -33.8688, lng: 151.2195 },
+        zoom: 13,
+        mapTypeId: "roadmap"
+    });
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+    let markers = [];
+    searchBox.addListener("places_changed", function () {
+    const places = searchBox.getPlaces();
 
-    map = new google.maps.Map(document.getElementById('map'), options);
-
-    infoWindow = new google.maps.InfoWindow;
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function () {
-            var position = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            }
-            }, function () {
-            handleLocationError('Geolocation service failed', map.center());
+    if (places.length === 0) {
+        return;
+    }
+    markers.forEach(marker => {
+        marker.setMap(null);
+    
+    markers = [];
+    });
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach(place => {
+        if (!place.geometry) {
+            console.log("Returned place contains no geometry");
+            return;
+        }
+        const icon = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+        };
+        // Create a marker for each place.
+        markers.push(
+            new google.maps.Marker({
+                map,
+                icon,
+                title: place.name,
+                position: place.geometry.location
             })
-            } else {
-                handleLocationError('No geolocation available', map.center());
+        );
 
-            }
-            }
+        if (place.geometry.viewport) {
+            // Only geocodes have viewport.
+            bounds.union(place.geometry.viewport);
+        } else {
+            bounds.extend(place.geometry.location);
+        }
+    });
+    map.fitBounds(bounds);
 
-            function handleLocationError(content, position) {
-                info.window.setPosition(position);
-                infoWindow.setContent(content);
-                infoWindow.open(map);
-            }
-        
-
-
-
-
-
-
-
+    });
+}
